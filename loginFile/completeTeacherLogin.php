cda23f572e79d927ad402c8b68451051c51ab3ce
Munziu <158445162+Munziu@users.noreply.github.com>
2024-10-03 11:13:10 +0300

    Update completeTeacherLogin.php

--- loginFile/completeTeacherLogin.php
+++ loginFile/completeTeacherLogin.php
@@ -1,44 +1,46 @@
 <?php
-    if($_SERVER["REQUEST_METHOD"]=="POST"){
-        include "../inc/databaseConnection.php";
-        include "../inc/formValidation.php";
-        include "../inc/checkingLoginStatus.php";
-        
-        $userId = validateFormData($_POST["userId"]);
-        $instituteCode = validateFormData($_POST["instituteCode"]);
-        $password = validateFormData($_POST["password"]);
-       
-        $database = "login_info";
-        $table = "teacher_login";
-            
-        $flag = checkUserLoginStatus($conn,$database,$table,$userId,$instituteCode,$password);
-               
-        if($flag == 1){
-            $conn->close();
-            echo "<script>alert('Successfully Login');</script>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include "../inc/databaseConnection.php";
+    include "../inc/formValidation.php";
+    include "../inc/checkingLoginStatus.php";
     
-            session_start();
-            $_SESSION["teacherStatus"]=true;
-            $_SESSION["userId"]=$userId;
-            $_SESSION["instituteCode"]=$instituteCode;
-            echo "<script>window.location.href='../homeFile/teacherHome.php';</script>";
-            die();
-        }
-        else if($flag == 2){
-            $conn->close();
-            echo "<script>alert('Incorrect Password or UserId or Institute Code, try again');";
-            echo "window.location.href='teacherLogin.php';</script>";
-            die();
-        }
-        else{
-            $conn->close();
-            echo "<script>alert('Error Occured');";
-            echo "window.location.href='teacherLogin.php';</script>";
-            die();
-        }
+    // Sanitize and validate form data
+    $userId = validateFormData($_POST["userId"]);
+    $instituteCode = validateFormData($_POST["instituteCode"]);
+    $password = validateFormData($_POST["password"]);
+    
+    // The database and table information
+    $database = "login_info";
+    $table = "teacher_login";
+    
+    // Check login status
+    $flag = checkUserLoginStatus($conn, $database, $table, $userId, $instituteCode, $password);
+    
+    if ($flag == 1) {
+        $conn->close();
+        echo "<script>alert('Successfully Logged In');</script>";
+        
+        // Start session and set login status
+        session_start();
+        $_SESSION["teacherStatus"] = true;
+        $_SESSION["userId"] = $userId;
+        $_SESSION["instituteCode"] = $instituteCode;
+        
+        // Redirect to teacher's home page
+        echo "<script>window.location.href='../homeFile/teacherHome.php';</script>";
+        die();
+    } elseif ($flag == 2) {
+        $conn->close();
+        echo "<script>alert('Incorrect Password or UserId or Institute Code, try again');</script>";
+        echo "<script>window.location.href='teacherLogin.php';</script>";
+        die();
+    } else {
+        $conn->close();
+        echo "<script>alert('Error Occurred');</script>";
+        echo "<script>window.location.href='teacherLogin.php';</script>";
+        die();
     }
-    else {
-        header("Location: ../index.php");
-        exit();
-    }
-?>
+} else {
+    header("Location: ../index.php");
+    exit();
+}
